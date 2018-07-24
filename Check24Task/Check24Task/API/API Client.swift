d1167800b62ea711b82ca886c34529e46111f005
akabel92 <ali.kabil92@gmail.com>
2018-07-24 12:18:29 +0200

    add AllProductTableViewController

--- Check24Task/Check24Task/API/API Client.swift
+++ Check24Task/Check24Task/API/API Client.swift
@@ -13,18 +13,6 @@ typealias ErrorInfo = (error: NSError?, isCancelled: Bool)
 
 class APIClient: HTTPClient {
     
-    /**
-     requestAPI with params
-     */
-//    static func requestAPI<T:JSONMappable>(_ method: Alamofire.HTTPMethod, url: URL, params: [String:AnyObject], headers: [String:String]) -> Task<Progress, T, NSError> {
-//
-//        return super.request(method, url: url, params: params, headers: headers)
-//
-//    }
-    
-    /**
-     requestAPI without params
-     */
     static func requestAPI<T:JSONMappable>(_ method: Alamofire.HTTPMethod, url: URL) -> Task<Progress, T, NSError> {
         return super.request(method, url: url)
         
