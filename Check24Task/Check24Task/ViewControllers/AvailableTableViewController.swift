9e9c3b628a28ab0586393d543bab4d173a13b76e
akabel92 <ali.kabil92@gmail.com>
2018-07-24 15:44:40 +0200

    show available products

--- Check24Task/Check24Task/ViewControllers/AvailableTableViewController.swift
+++ Check24Task/Check24Task/ViewControllers/AvailableTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AllProductTableViewController.swift
+//  AvailableTableViewController.swift
 //  Check24Task
 //
 //  Created by Ali Kabel on 24.07.18.
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class AllProductTableViewController: UITableViewController {
+class AvailableTableViewController: UITableViewController {
 
     // MARK:- Initilization
     
     let reuseIdentifier = "ProductTableViewCell"
-    var allProducts = [Product]()
+    var availableProducts = [Product]()
     // MARK:- View Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationController?.navigationBar.topItem?.title = "CHECK24"
         addRefereshController()
         registerCell()
-        getAllProducts()
+        getAllAvailableProducts()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,32 +35,34 @@ class AllProductTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allProducts.count
-    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Allee Products"
+        return "Availabel Products"
     }
     
     override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         return "@2018 Check24"
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ProductTableViewCell
-
-        cell.productNameLabel.text = allProducts[indexPath.row].name
-        cell.productDesriptionLabel.text = allProducts[indexPath.row].shortDescription
-        cell.priceLabel.text = String(allProducts[indexPath.row].price.value)
-        cell.priceLabel.text = "Price: \(allProducts[indexPath.row].price.value)"
-        cell.dateLabel.text = allProducts[indexPath.row].getDate(allProducts[indexPath.row].releaseDate)
-        return cell
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return availableProducts.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ProductTableViewCell
+        
+        cell.productNameLabel.text = availableProducts[indexPath.row].name
+        cell.productDesriptionLabel.text = availableProducts[indexPath.row].shortDescription
+        cell.priceLabel.text = String(availableProducts[indexPath.row].price.value)
+        cell.priceLabel.text = "Price: \(availableProducts[indexPath.row].price.value)"
+        cell.dateLabel.text = availableProducts[indexPath.row].getDate(availableProducts[indexPath.row].releaseDate)
+        return cell
+    }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "details", sender: self)
     }
@@ -69,14 +71,18 @@ class AllProductTableViewController: UITableViewController {
         if segue.identifier == "details" {
             if let indexPath = tableView.indexPathForSelectedRow{
                 let destinationController = segue.destination as! ProductDetailsViewController
-                destinationController.product = allProducts[indexPath.row]
+                destinationController.product = availableProducts[indexPath.row]
             }
         }
     }
     // MARK:- Product API
-    @objc func getAllProducts() {
+    @objc func getAllAvailableProducts() {
         ProdutctsAPI.getProducts().success{ productsResult in
-            self.allProducts = productsResult.products
+            for product in productsResult.products {
+                if product.available {
+                    self.availableProducts.append(product)
+                }
+            }
             self.tableView.reloadData()
             self.refreshControl?.endRefreshing()
         }
@@ -91,7 +97,8 @@ class AllProductTableViewController: UITableViewController {
     
     func addRefereshController() {
         refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(getAllProducts), for: .valueChanged)
+        refreshControl?.addTarget(self, action: #selector(getAllAvailableProducts), for: .valueChanged)
         refreshControl?.beginRefreshing()
     }
+
 }
