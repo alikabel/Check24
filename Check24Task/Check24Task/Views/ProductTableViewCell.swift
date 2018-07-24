357588af9c683a26aeb567f84024cfb4f541be21
akabel92 <ali.kabil92@gmail.com>
2018-07-24 11:51:40 +0200

    add custom cell view

---
+++ Check24Task/Check24Task/Views/ProductTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  ProductTableViewCell.swift
+//  Check24Task
+//
+//  Created by Ali Kabel on 24.07.18.
+//  Copyright © 2018 Ali Kabel. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var productDesriptionLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
